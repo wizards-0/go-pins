aa8df9473a7645fb7849d179271eb945709b6795
Ameya Lokras <ameyalokras@gmail.com>
2025-06-28 14:00:23 +0530

    Automated commit for version increment

--- publish.sh
+++ publish.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-#last published version
-#git tag $1 #vx.x.x
-#git push origin $1
-#GOPROXY=proxy.golang.org go list -m github.com/wizards-0/go-pins@$1
+
 mainVersionLine=$(grep "main.version" version.ini)
 mainVersionKeyValue=(${mainVersionLine//=/ })
 mainVersion=${mainVersionKeyValue[1]}
 mainVersionParts=(${mainVersion//./ })
 mainVersionPatchOld=${mainVersionParts[2]}
 mainVersionPatchNew=$((mainVersionPatchOld+1))
-mainVersionLineNew="main.version=${mainVersionParts[0]}.${mainVersionParts[1]}.$mainVersionPatchNew"
+mainVersionNew="${mainVersionParts[0]}.${mainVersionParts[1]}.$mainVersionPatchNew"
+mainVersionLineNew="main.version=$mainVersionNew"
 sed -i "s/${mainVersionLine}/${mainVersionLineNew}/g" ./version.ini
-cat version.ini
+git commit -a -u -m "Automated commit for version increment"
+git tag $mainVersionNew
+git push origin $mainVersionNew
+GOPROXY=proxy.golang.org go list -m github.com/wizards-0/go-pins@$mainVersionNew
